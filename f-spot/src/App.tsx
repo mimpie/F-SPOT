af6e86e9282c06c4bb092edbc5bde19c9c4a7602
mimpie <yesl3715@naver.com>
2023-11-24 16:16:45 +0900

    init setting

---
+++ f-spot/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import KakaoMap from "./components/map";
+
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Your App Title</h1>
+      <KakaoMap />
+    </div>
+  );
+};
+
+export default App;
